14226f6045569043a4592dab8a77dab410383450
Klay Clarke <klayaclarke@gmail.com>
2021-11-24 09:15:27 -0500

    use try except statement to catch errors caused by illegal input (user input that includes numbers) - if user enters numbers, user will be prompted to enter letters, these prompts will return until the user complies with directions

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
 import pandas
 
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
-
-for (index, row) in df.iterrows():
-    pass
 
 nato_dict = {row.letter: row.code for index, row in df.iterrows()}
 
-
-# # TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 # print(nato_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input('Enter a word:').upper()
-list_of_letters = [letter for letter in user_input]
-len(list_of_letters)
+success = False
 
-nato_translation_list = [nato_dict[letter] for letter in list_of_letters]
-print(nato_translation_list)
+while not success:
+
+    user_input = input('Enter a word:').upper()
+    list_of_letters = [letter for letter in user_input]
+
+    try:
+        nato_translation_list = [nato_dict[letter] for letter in list_of_letters]
+    except KeyError:
+        print('Sorry, only letters in the alphabet please!')
+    else:
+        print(nato_translation_list)
+        success = True
